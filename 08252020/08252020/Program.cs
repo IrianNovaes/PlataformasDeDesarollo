fcb69056b43988c4954b72efd0a7db3b6131395f
Irian Novaes <irian.novaes@outlook.com>
2020-08-25 23:00:33 -0300

    clase 08/25

--- 08252020/08252020/Program.cs
+++ 08252020/08252020/Program.cs
@@ -6,7 +6,31 @@ namespace _08252020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            //int i = 0;
+            //while (i>10)
+            //{
+            //    Console.WriteLine(++i);
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(i);
+
+            //} while (i > 10);
+
+            int misteryNumber = 0;
+            int trys = 0;
+
+            while(trys < 3 || misteryNumber != 40)
+            {
+                trys++;
+                Console.WriteLine("enter the mistery number: ");
+                misteryNumber = int.Parse(Console.ReadLine());
+            };
+
+            
         }
     }
 }
