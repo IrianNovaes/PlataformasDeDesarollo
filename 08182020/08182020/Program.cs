4495f6b3b54a46f0ecefcb6bdb06f21c1e7d4c5c
Irian Novaes <irian.novaes@outlook.com>
2020-08-25 21:22:20 -0300

    Creacion e inicializacion del Repositorio

---
+++ 08182020/08182020/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _08182020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0, numero = 0, contador = 11;
+            
+            var random = new Random();
+
+            Console.WriteLine("Enter your name:");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine($"Hello {nombre}, Lets Play a game");
+            Console.ReadLine();
+            
+            Console.WriteLine("Please enter 10 different numbers untill you reach the mistery number.");
+
+            for (int i = 0; i < contador; i++)
+            {
+                if (i >= 10)
+                {
+                    Console.WriteLine("that was your 10th number, You have no more options, byeee");
+                }
+                else if (suma < 50 )
+                {
+                    Console.WriteLine("Enter you number");
+                    numero = int.Parse(Console.ReadLine());
+
+                    if (numero <= 50)
+                    {
+                        suma += numero;
+                    }
+                    else if (suma > 50 || numero > 50)
+                    {
+                        contador = 1;
+                        Console.WriteLine("You have exploded.");
+                    }
+                } else if (numero == 50 || suma == 50)
+                    {
+                        contador = 1;
+                        Console.WriteLine("Congratulations the mistery number was 50");
+                    }
+
+                
+            }
+        }
+    }
+}
